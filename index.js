5a7656d38a0bac9f76a0ed3f7d6e012633e79518
xaviguasch <xaviguasch@gmail.com>
2020-09-14 12:02:13 +0200

    solved first challenge

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 console.log('hey there')
+/* 
+    Library
+    
+    Fetch the collection of books 
+        from books.json, assign each 
+        a unique id
+        
+    Create a div to represent a book
+        using Grid & grid-template-areas 
+        build a book component
+        display all books using Flexbox
+*/
+
+async function getBooks() {
+  let response = await fetch('books.json')
+  let books = await response.json()
+  let n = 1
+  return books.map((book) => {
+    book.id = n
+    n += 1
+    return book
+  })
+}
+
+function getBookHtml(book) {
+  return `<div class="my-book">
+      <div class="my-book-cover">${book.title}</div>
+      <div class="my-book-spine"></div>
+      <div class="my-book-footer"></div>
+  </div>`
+}
+
+getBooks().then((books) => {
+  document.body.innerHTML = `<div class="my-library">
+  ${books.map(getBookHtml).join('')}
+</div>`
+})
